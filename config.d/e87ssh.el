6e3baa561924ea306c59d2761005916321def3ad
Weihua Li <lwh@local>
2017-03-18 22:55:47 +0800

    add term/ssh command

---
+++ config.d/e87ssh.el
@@ -0,0 +1,9 @@
+(defun ssh (args)
+  "Connect to a remote host by SSH."
+  (interactive "sssh ")
+  (let ((switches (split-string-and-unquote args)))
+    (defvar term-term-name "xterm")
+    (set-buffer (apply 'make-term "ssh" "ssh" nil switches))
+    (term-mode)
+    (term-char-mode)
+    (switch-to-buffer "*ssh*")))
