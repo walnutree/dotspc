899d315ced1557d07c087f1d7fadc6a2bcaf87fc
lll <xx@email.com>
2019-05-24 10:44:49 +0800

    some misc modifies, org-mod/magit/memo.org

--- config.d/e19org-mode.el
+++ config.d/e19org-mode.el
@@ -4,3 +4,9 @@
 (eval-after-load 'evil-org #'my-org-mode-hook)
 
 (setq org-bullets-bullet-list '("○" "●" "◦" "♠" "♣" "♥" "♦"))
+
+;; sub/super text must round with {}, to avoid lots of _ usage in org text
+;; https://stackoverflow.com/questions/26636562/org-mode-weirdness-with-org-export-with-sub-superscripts
+;; http://orgmode.org/manual/Subscripts-and-superscripts.html#Subscripts-and-superscripts
+(setq org-use-sub-superscripts '{})
+(setq org-export-with-sub-superscripts '{})
--- config.d/e61magit.el
+++ config.d/e61magit.el
@@ -15,31 +15,31 @@
 (setq-default git-commit-mode-hook '(turn-on-auto-fill))
 (global-set-key (kbd "M-r") 'magit-status)
 
-;; (spacemacs|define-transient-state smerge
-;;   :title "Smerge Transient State"
+(spacemacs|define-transient-state smerge
+  :title "Smerge Transient State"
 
-;;     :doc "
-;;  Movement^^^^               Merge Action^^           Other
-;;  ---------------------^^^^  -------------------^^    -----------
-;;  [_n_/_p_]  next/prev hunk  [_b_] keep base          [_u_] undo
-;;  [_j_/_k_]  move up/down    [_m_] keep mine          [_R_] refine
-;;  ^^^^                       [_a_] keep all           [_q_] quit
-;;  ^^^^                       [_o_] keep other
-;;  ^^^^                       [_c_] keep current
-;;  ^^^^                       [_C_] combine with next"
-;;     :bindings
-;;     ("n" smerge-next)
-;;     ("p" smerge-prev)
-;;     ("k" smerge-prev)
-;;     ("j" evil-next-line)
-;;     ("k" evil-previous-line)
-;;     ("a" smerge-keep-all)
-;;     ("b" smerge-keep-base)
-;;     ("m" smerge-keep-mine)
-;;     ("o" smerge-keep-other)
-;;     ("c" smerge-keep-current)
-;;     ("C" smerge-combine-with-next)
-;;     ("R" smerge-refine)
-;;     ("u" undo-tree-undo)
-;;     ("q" nil :exit t)
-;;     )
+    :doc "
+ Movement^^^^               Merge Action^^           Other
+ ---------------------^^^^  -------------------^^    -----------
+ [_n_/_p_]  next/prev hunk  [_b_] keep base          [_u_] undo
+ [_j_/_k_]  move up/down    [_m_] keep mine          [_R_] refine
+ ^^^^                       [_a_] keep all           [_q_] quit
+ ^^^^                       [_o_] keep other
+ ^^^^                       [_c_] keep current
+ ^^^^                       [_C_] combine with next"
+    :bindings
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("k" smerge-prev)
+    ("j" evil-next-line)
+    ("k" evil-previous-line)
+    ("a" smerge-keep-all)
+    ("b" smerge-keep-base)
+    ("m" smerge-keep-mine)
+    ("o" smerge-keep-other)
+    ("c" smerge-keep-current)
+    ("C" smerge-combine-with-next)
+    ("R" smerge-refine)
+    ("u" undo-tree-undo)
+    ("q" nil :exit t)
+    )
